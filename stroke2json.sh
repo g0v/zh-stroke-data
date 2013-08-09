dce08a2bc3a59dbc146f16ac65f948efcf3bd116
caasi Huang <caasi.igd@gmail.com>
2013-08-09 21:11:02 +0800

    fix json format bug which caused by wrong indent; output the progress of stroke2json.sh

--- stroke2json.sh
+++ stroke2json.sh
@@ -1,3 +1,4 @@
-for f in `find ./utf8/ -type f`; do
-  node ./stroke2json.js $f > ./json/${f:7:4}.json
+for f in `find ./utf8/ -type f | sort`; do
+  echo parsing $f
+  node ./stroke2json.js $f > ./json/${f:7:4}.json || break
 done
