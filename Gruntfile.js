d71cc3c4a3ee9cb029d4c3924f522a85cf931414
caasi Huang <caasi.igd@gmail.com>
2013-10-22 23:30:54 +0800

    no source maps for now

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,7 @@ module.exports = function(grunt) {
     coffee: {
       compile: {
         options: {
-          sourceMap: true
+          sourceMap: false
         },
         files: {
           /* both */
